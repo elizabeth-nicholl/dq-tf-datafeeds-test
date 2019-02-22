3dcd599c3e6c4803fb5546602500b909b28b6db4
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-22 19:34:57 +0000

    Test

--- variables.tf
+++ variables.tf
@@ -1,11 +1,27 @@
-variable "data_feeds_cidr_block" {}
-variable "peering_cidr_block" {}
-variable "appsvpc_id" {}
-variable "opssubnet_cidr_block" {}
-variable "data_pipe_apps_cidr_block" {}
-variable "data_feeds_cidr_block_az2" {}
-variable "az" {}
-variable "az2" {}
+variable "data_feeds_cidr_block" {
+  default = "10.1.4.0/24"
+}
+variable "peering_cidr_block" {
+  default = "10.3.0.0/16"
+}
+variable "appsvpc_id" {
+  default = "10.1.0.0/16"
+}
+variable "opssubnet_cidr_block" {
+  default = "10.8.0.0/16"
+}
+variable "data_pipe_apps_cidr_block" {
+  default = "10.1.8.0/24"
+}
+variable "data_feeds_cidr_block_az2" {
+  default = "10.1.5.0/24"
+}
+variable "az" {
+  default = "a"
+}
+variable "az2" {
+  default = "b"
+}
 
 variable "datafeed_rds_db_name" {
   default = "ef_db"
